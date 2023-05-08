55dd8d322808de068d05d93b3d0a4d5041717c38
Enes OKURTERZİ <113862251+enesokurterzi@users.noreply.github.com>
2023-05-08 15:22:01 +0300

    Update GameViewModel.kt

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -5,6 +5,9 @@ import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
     init {
         Log.d("GameFragment", "GameViewModel created!")
         getNextWord()
@@ -15,9 +18,6 @@ class GameViewModel : ViewModel() {
     private lateinit var _currentScrambledWord: String
     val currentScrambledWord: String
         get() = _currentScrambledWord
-
-    private var wordsList: MutableList<String> = mutableListOf()
-    private lateinit var currentWord: String
 
     private fun getNextWord() {
         currentWord = allWordsList.random()
@@ -37,5 +37,15 @@ class GameViewModel : ViewModel() {
         }
     }
 
+/*
+* Returns true if the current word count is less than MAX_NO_OF_WORDS.
+* Updates the next word.
+*/
+    fun nextWord(): Boolean {
+        return if (currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
 
 }
